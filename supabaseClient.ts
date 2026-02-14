3e05f2f31c91bcef63fcf5e0e5b1adf9ee724d54
山田秀和 <matunokihanten@gmail.com>
2026-02-14 23:53:15 +0900

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://jgqikbwaxxeullzerhek.supabase.co';
-const supabaseAnonKey = 'ここにanon_keyを貼り付けてください';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpncWlrYndheHhldWxsemVyaGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEwNDcwNjMsImV4cCI6MjA4NjYyMzA2M30.mJCqTt_O4Q0hLImMQLAZa_I90L2jfE7XUmeWaGkLxK0';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
