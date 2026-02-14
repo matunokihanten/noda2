ad4195a1a29449aedaff830229704a44babc6741
山田秀和 <matunokihanten@gmail.com>
2026-02-14 22:22:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   preview: {
-    // どのURLからのアクセスも許可する設定にします
-    allowedHosts: true
-  },
-  server: {
-    allowedHosts: true
+    // RenderのURLからのアクセスを許可します
+    allowedHosts: ['noda2-2.onrender.com']
   }
 })
