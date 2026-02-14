aeb0470fc2860a8b5e2dc681b8c1f16aef4841ee
山田秀和 <matunokihanten@gmail.com>
2026-02-14 22:19:27 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    // どのURLからのアクセスも許可する設定にします
+    allowedHosts: true
+  },
+  server: {
+    allowedHosts: true
+  }
 })
